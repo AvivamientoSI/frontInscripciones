5c20486be59a9871dc8e7394a8ef84afed40e081
AvivamientoSI <166962463+AvivamientoSI@users.noreply.github.com>
2025-03-08 10:35:19 -0300

    Update schools.js

--- src/store/schools.js
+++ src/store/schools.js
@@ -6,7 +6,7 @@ export const useSchoolsStore = create((set) => ({
     setSchools: (schools) => set({schools}),
 
     fetchSchools: async () => {
-        const response = await fetch('http://localhost:8080/api/schools');
+        const response = await fetch('https://inscripciones-i4tm.onrender.com/api/schools');
         const data = await response.json();
         
         set({schools: data.data});
