35ed9c953d09dc0931ad5cb3ef1fd80860265acc
Joaco <joaquineliasvega@gmail.com>
2025-03-28 23:45:36 -0300

    cerrando ABC

--- src/components/ifComponent.jsx
+++ src/components/ifComponent.jsx
@@ -5,7 +5,7 @@ import EsCard, {EsCardTable} from "./esCard";
 const IfComponent = ({school, index}) => {
     try {
             if (school.name === "ABC") {
-                return <AbcCard key={school.id || index} school={school} />;
+                return //<AbcCard key={school.id || index} school={school} />
             } else if (school.name === "Escuela de Vida") {
                 return <EsCard key={school.id || index} school={school} />;
             } else if (school.name === "Discipulado") {
